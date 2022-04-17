53e8e91964cd7301726505772f270bdc3b6767ac
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-04-17 11:02:41 -0300

    test: testing component Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import { Header } from "../components/Header/Header";
+import { Header } from "../components/Header";
 import { SessionProvider as NextAUthProvider } from "next-auth/react";
 import "../style/global.scss";
 
