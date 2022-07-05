cb759043c0d533656a322602d330c4fa7380e012
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-05 06:58:39 -0300

    chore: update error message

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,13 +1,12 @@
 import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 import { api } from "../../services/api";
 import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
 
 export function SubscribeButton() {
   const { data: session } = useSession();
   const router = useRouter();
-
   async function handleSubscribe() {
     if (!session) {
       signIn("github");
@@ -22,19 +21,17 @@ export function SubscribeButton() {
     try {
       const response = await api.post("/subscribe");
       const { sessionId } = response.data;
+
       const stripe = await getStripeJs();
 
       await stripe.redirectToCheckout({ sessionId });
-    } catch (error) {
-      alert(error);
+    } catch (err) {
+      alert(err.message);
     }
   }
+
   return (
-    <button
-      onClick={handleSubscribe}
-      type="button"
-      className={styles.subscribeButton}
-    >
+    <button onClick={handleSubscribe} className={styles.subscribeButton}>
       Subscribe now
     </button>
   );
